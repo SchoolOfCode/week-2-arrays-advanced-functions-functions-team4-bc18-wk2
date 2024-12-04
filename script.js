6994ed44f0f0220a314f94f3ef3a1da7a9dd6af3
ash <menicce4@gmail.com>
2024-12-04 16:55:44 +0000

    added.map

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ console.log("Hello World!");
 function greeting(name) {
     console.log(`Hello ${name}`);
 }
+
+//Create a higher order function
+//Create a function that accepts:
+//A callback function
+//Three name parameters
+//It should call the callback function (in this case, the greeting function we made), passing each name
+
+let names = [
+    "Ashton", "Connor", "Giuseppe"
+];
+
+names.map(greeting(names))
+{
+    return  `hello` + names
+};
